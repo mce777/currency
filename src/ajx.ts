45b1a848245a860af9e2fbe048528c3b16ede932
marebe <mark.eberhard@sinnerschrader.com>
2016-10-06 17:32:17 +0200

    current state

---
+++ src/ajx.ts
@@ -0,0 +1,40 @@
+/// <reference path='../typings/index.d.ts' />
+
+// export function loadAPI(url: string, callback: any): any {
+// 	let xmlhttp = new XMLHttpRequest();
+// 	xmlhttp.onreadystatechange = () => {
+// 		let data = JSON.parse(xmlhttp.responseText);
+// 		if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+// 			try {
+// 				callback(data);
+// 			} catch (err) {
+// 				console.log(err.message + ' in ' + xmlhttp.responseText);
+// 				return;
+// 			}
+// 		}
+// 	};
+// 	xmlhttp.open('GET', url, true);
+// 	xmlhttp.send();
+// }
+
+export function loadAPI(url: string) {
+	return new Promise((resolve, reject) => {
+		const xmlhttp = new XMLHttpRequest();
+
+		xmlhttp.onreadystatechange = () => {
+			const data = JSON.parse(xmlhttp.responseText);
+			resolve(data);
+		};
+
+		xmlhttp.onerror = () => {
+			const message = 'no can haz';
+			reject(new Error(message));
+		};
+
+		xmlhttp.open('GET', url, true);
+		xmlhttp.send();
+
+	})
+
+}
+console.log(loadAPI('http://api.fixer.io/latest?base=EUR'));
--- test/currencyTests.ts
+++ test/currencyTests.ts
@@ -1,5 +1,6 @@
 import { Converter } from '../src/currency';
 import { assert } from 'chai';
+import { loadAPI } from '../src/ajx';
 
 describe('Testing of the Currency Converter', () => {
 	describe('convert()', () => {
@@ -11,7 +12,7 @@ describe('Testing of the Currency Converter', () => {
 		});
 
 		it('should not except values < 0', () => {
-			let ctest = new Converter(0.6642);
+			const ctest = new Converter(0.6642);
 
 			assert.throws(() => ctest.convert(-10), 'Negative values are not permitted');
 		});
@@ -19,17 +20,28 @@ describe('Testing of the Currency Converter', () => {
 
 	describe('convertBack()', () => {
 		it('should convert back to original currency', () => {
-			let cbtest = new Converter(0.8903);
-			let cbtestResult = cbtest.convert(45);
-			let convertBackResult = cbtest.convertBack(cbtestResult);
+			const cbtest = new Converter(0.8903);
+			const cbtestResult = cbtest.convert(45);
+			const convertBackResult = cbtest.convertBack(cbtestResult);
 
 			assert.strictEqual(convertBackResult, 45, 'Did not convert back correctly');
 		});
 
 		it('should not except values < 0', () => {
-			let cbtest = new Converter(0.8903);
+			const cbtest = new Converter(0.8903);
 
 			assert.throws(() => cbtest.convertBack(-10), 'Negative values are not permitted');
 		});
 	});
+
+	describe('loadAPI()', () => {
+		it('should retrieve data from an API', () => {
+			const apiTest = loadAPI('http://api.fixer.io/latest?base=EUR');
+			const apiTestResult = apiTest['base'];
+
+			assert.equal(apiTestResult, 'EUR', 'Did not convert back correctly');
+		});
+
+	});
+
 });
