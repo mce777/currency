c246f3b3b64d9dfc8b45f29e80928a0e740b2ace
marebe <mark.eberhard@sinnerschrader.com>
2016-12-20 17:04:12 +0100

    deletions

--- styleguide.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-module.exports = {
-	title: 'My Great Style Guide',
-	components: './components/**/*.js',
-	sections: [
-		{ name: 'Introduction', content: './docs/introduction.md' }
-		// { name: 'UI Components', content: './docs/ui.md', components: 'lib/components/ui/*.js' },
-	]
-};
